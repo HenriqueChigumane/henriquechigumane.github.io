8f404c87b4d42a88e49003c361708cd711f7228b
Henrque Chigumane <henriquechigumane@outlook.com>
2020-03-03 23:32:56 +0200

    fixing bug and optmize  image from thesigner

--- compress-images.js
+++ compress-images.js
@@ -1,11 +1,11 @@
   
     var compress_images = require('compress-images');
 
-    const INPUT_path_to_your_images = 'assets/img/thesigner/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
-    const OUTPUT_path = 'assets/img/temp';
+    const INPUT_path_to_your_images = 'assets/img/portfolio/*.{jpg,JPG,png,jpeg,JPEG,svg,gif}';
+    const OUTPUT_path = 'assets/img/temp/';
     
     compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
-                                                {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+                                                {jpg: {engine: 'mozjpeg', command: ['-quality', '100']}},
                                                 {png: {engine: 'pngquant', command: ['--quality=20-50']}},
                                                 {svg: {engine: 'svgo', command: '--multipass'}},
                                                 {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(){
